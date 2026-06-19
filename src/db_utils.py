3bb98ec73ddc13ed70f855aba3a81e637269367b
Eduard Chalovski <eduard.chalovski@gmail.com>
2026-06-19 12:09:46 +0200

    add: check if table_exists

--- src/db_utils.py
+++ src/db_utils.py
@@ -1,6 +1,8 @@
 import pypdf
 from langchain_core.documents import Document
 
+import psycopg
+from src.config import postgres_config
 
 # Below is a minimal helper for demonstration purposes.
 def load_pdf_pages(file_path: str) -> list[Document]:
@@ -12,3 +14,16 @@ def load_pdf_pages(file_path: str) -> list[Document]:
         )
         for i, page in enumerate(reader.pages)
     ]
+
+def table_exists(
+    table_name: str,
+    schema: str = "public",
+) -> bool:
+    with psycopg.connect(**postgres_config.psycopg_connection_kwargs) as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT to_regclass(%s) IS NOT NULL;",
+                (f"{schema}.{table_name}",),
+            )
+            result = cur.fetchone()
+            return bool(result[0])
